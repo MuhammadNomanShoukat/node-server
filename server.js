7a1c3bd23c7d4c8b7c577f6931e6c132e3ad1568
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2024-08-07 13:06:25 +0500

    server.js added

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require('cors');
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*', // Allow all origins (for testing purposes, you can restrict this in production)
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -21,3 +30,7 @@ app.get('/', (req, res) => {
 httpServer.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+module.exports = (req, res) => {
+  app(req, res);
+};
