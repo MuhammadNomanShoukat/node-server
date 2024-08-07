d7995d346b77e0b4bd3c2afe8d3c06fd4a232381
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2024-08-07 14:17:42 +0500

    server.js added

--- server.js
+++ server.js
@@ -1,38 +1,22 @@
-const express = require("express");
-const { createServer } = require("http");
+const { Hono } = require("hono");
+const { serve } = require("@hono/node-server");
 const { Server } = require("socket.io");
-const path = require('path');
-const cors = require('cors');
 
-const app = express();
-const httpServer = createServer(app);
+const app = new Hono();
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: '*',
-    methods: ["GET", "POST"]
-  }
+const httpServer = serve({
+    fetch: app.fetch,
+    port: 3000,
 });
 
-app.use(cors());
-app.use('/socket.io', express.static(path.join(__dirname, 'node_modules/socket.io/client-dist')));
+const io = new Server(httpServer, {
+    /* options */
+});
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log('a user connected');
   
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-httpServer.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-
-module.exports = (req, res) => {
-  app(req, res);
-};
