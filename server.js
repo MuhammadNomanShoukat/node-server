8439dcaa40eda35d9492b40ceb87d477b593a0f6
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2024-08-07 14:25:46 +0500

    server.js added

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-const { Hono } = require("hono");
-const { serve } = require("@hono/node-server");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-const app = new Hono();
-
-const httpServer = serve({
-    fetch: app.fetch,
-    port: 3000,
-});
-
-const io = new Server(httpServer, {
-    /* options */
-});
+const httpServer = createServer();
+const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  console.log('a user connected');
+  console.log('A user connected');
+
   
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('User disconnected');
+
   });
 });
+
+httpServer.listen(3000);
